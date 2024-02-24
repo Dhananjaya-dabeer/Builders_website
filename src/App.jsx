83dde20f8296b01479cb91fc919594241ea172a5
Dhananjaya-dabeer <dabeerdhananja123@gmail.com>
2024-02-25 01:26:24 +0530

    done with navbar and header components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Webpage from "./pages/Webpage";
+
+function App() {
+  return (
+    <>
+      <Webpage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Webpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import PageHeader from "../components/PageHeader";
+
+function Webpage() {
+  return (
+    <div>
+      <Navbar />
+      <PageHeader />
+    </div>
+  );
+}
+
+export default Webpage;
